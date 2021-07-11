3a88736604b50f3016ce49ae2e4e7834f0616680
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2021-07-11 17:04:54 +0300

    Navigating to filtered events programmatically

--- components/events/EventSearch.js
+++ components/events/EventSearch.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import Button from "../ui/button";
 import classes from "./EventSearch.module.css";
 
@@ -18,12 +18,23 @@ const EventSearch = (props) => {
         { id: 12, label: "December" },
     ];
 
+    const yearInputRef = useRef();
+    const monthInputRef = useRef();
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        const year = yearInputRef.current.value;
+        const month = monthInputRef.current.value;
+
+        props.onSearch(year, month);
+    };
+
     return (
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitHandler}>
             <div className={classes.controls}>
                 <div className={classes.control}>
                     <label htmlFor="year">Year</label>
-                    <select name="" id="year">
+                    <select name="" id="year" ref={yearInputRef}>
                         {[2020, 2021, 2022, 2023].map((year) => (
                             <option key={year} value={year}>
                                 {year}
@@ -33,7 +44,7 @@ const EventSearch = (props) => {
                 </div>
                 <div className={classes.control}>
                     <label htmlFor="month">Month</label>
-                    <select name="" id="month">
+                    <select name="" id="month" ref={monthInputRef}>
                         {months.map((month) => (
                             <option key={month.id} value={month.id}>
                                 {month.label}
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { useRouter } from "next/router";
 import EventList from "../../components/events/EventList";
 import { getAllEvents } from "../../dummy-data";
 import EventSearch from "../../components/events/EventSearch";
 
 const AllEventsPage = () => {
     const events = getAllEvents();
+    const router = useRouter();
+
+    const findEventsHandler = (year, month) => {
+        const fullPath = `/events/${year}/${month}`;
+        router.push(fullPath);
+    };
 
     return (
         <>
-            <EventSearch />
+            <EventSearch onSearch={findEventsHandler} />
             <EventList items={events} />
         </>
     );
