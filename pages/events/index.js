567385b420d4a3a53ef11a1730472914bb3d57f6
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2021-09-22 09:53:20 +0300

    Working on the "All Events" Page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,24 +1,35 @@
 import React from "react";
 import { useRouter } from "next/router";
 import EventList from "../../components/events/EventList";
-import { getAllEvents } from "../../dummy-data";
+import { getAllEvents } from "../../helpers/api-util";
 import EventSearch from "../../components/events/EventSearch";
 
-const AllEventsPage = () => {
-    const events = getAllEvents();
-    const router = useRouter();
+const AllEventsPage = (props) => {
+  const { events } = props;
+  const router = useRouter();
 
-    const findEventsHandler = (year, month) => {
-        const fullPath = `/events/${year}/${month}`;
-        router.push(fullPath);
-    };
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  };
 
-    return (
-        <>
-            <EventSearch onSearch={findEventsHandler} />
-            <EventList items={events} />
-        </>
-    );
+  return (
+    <>
+      <EventSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </>
+  );
 };
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events,
+    },
+    revalidate: 60,
+  };
+}
 
 export default AllEventsPage;
