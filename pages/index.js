0dc9a7a51ef3e38b31be9cc25702e216a4a6b0c9
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2021-09-28 22:03:48 +0300

    Configuring the "head" Content

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { getFeaturedEvents } from "../helpers/api-util";
 import EventList from "../components/events/EventList";
+import Head from 'next/head'
 
 const HomePage = (props) => {
   return (
     <div>
+      <Head>
+        <title>NextJS Events Application</title>
+        <meta name="description" content="A NextJS application to handle events" />
+      </Head>
       <EventList items={props.events} />
     </div>
   );
