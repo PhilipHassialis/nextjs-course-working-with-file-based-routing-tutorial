7ef08b4019e33bc871685e8879b03e4c522f5b49
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2021-10-01 00:47:30 +0300

    Optimizing Images with the "Next Image" Component & Feature

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 import classes from "./EventItem.module.css";
 import Button from "../ui/button";
 import DateIcon from "../icons/DateIcon";
@@ -6,43 +7,43 @@ import AddressIcon from "../icons/AddressIcon";
 import ArrowRightIcon from "../icons/ArrowRightIcon";
 
 const EventItem = (props) => {
-    const { title, image, date, location, id } = props;
+  const { title, image, date, location, id } = props;
 
-    const humanReadableDate = new Date(date).toLocaleDateString("el-GR", {
-        day: "numeric",
-        month: "long",
-        year: "numeric",
-    });
+  const humanReadableDate = new Date(date).toLocaleDateString("el-GR", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
 
-    const formattedAddress = location.replace(", ", "\n");
-    const exploreLink = `/events/${id}`;
+  const formattedAddress = location.replace(", ", "\n");
+  const exploreLink = `/events/${id}`;
 
-    return (
-        <li className={classes.item}>
-            <img src={`/${image}`} alt={title} />
-            <div className={classes.content}>
-                <div className={classes.summary}>
-                    <h2>{title}</h2>
-                    <div className={classes.date}>
-                        <DateIcon />
-                        <time>{humanReadableDate}</time>
-                    </div>
-                    <div className={classes.address}>
-                        <AddressIcon />
-                        <address>{formattedAddress}</address>
-                    </div>
-                </div>
-                <div className={classes.actions}>
-                    <Button link={exploreLink}>
-                        <span>Explore Event</span>
-                        <span className={classes.icon}>
-                            <ArrowRightIcon />
-                        </span>
-                    </Button>
-                </div>
-            </div>
-        </li>
-    );
+  return (
+    <li className={classes.item}>
+      <Image src={`/${image}`} alt={title} width={250} height={150} />
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <DateIcon />
+            <time>{humanReadableDate}</time>
+          </div>
+          <div className={classes.address}>
+            <AddressIcon />
+            <address>{formattedAddress}</address>
+          </div>
+        </div>
+        <div className={classes.actions}>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default EventItem;
